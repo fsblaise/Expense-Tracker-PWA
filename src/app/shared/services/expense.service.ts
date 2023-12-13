b117e55fe365ee9d5d8c88ef7161240f812293ae
Balázs Oláh <balazs.olah@p92.hu>
2023-12-13 16:54:59 +0100

    minor changes

--- src/app/shared/services/expense.service.ts
+++ src/app/shared/services/expense.service.ts
@@ -5,6 +5,7 @@ import { ref } from '@angular/fire/storage';
 import { firstValueFrom, map } from 'rxjs';
 import { Expense } from '../models/expense.model';
 import { Transaction } from '../models/transaction.model';
+import { arrayUnion } from '@angular/fire/firestore';
 
 const OCR_API = 'https://api.ocr.space/parse/image';
 
@@ -48,35 +49,52 @@ export class ExpenseService {
   async getExpense(month: string, userId: string) {
     const res = await firstValueFrom(this.fstore
       .collection<Expense>('Expenses', ref => ref
-      .where('userId', '==', userId)
-      .where('month', '==', month))
+        .where('userId', '==', userId)
+        .where('month', '==', month))
       .valueChanges());
 
     return res[0];
   }
 
-  async addExpense(transaction: any, userId: string) {
+  async addExpense(transaction: Transaction, userId: string) {
     // get current month and year
     const month = new Date().getMonth() + 1;
     const year = new Date().getFullYear();
+    const monthStr = year + '-' + month;
 
     // check if an expense representing the current month is already existing
-    const snapshot = await this.fstore.collection('Expenses', ref => ref
-      .where('month', '==', (year + '-' + month))
-      .where('userId', '==', userId)).valueChanges();
+    const snapshot = await firstValueFrom(this.fstore
+      .collection<Expense>('Expenses', ref => ref
+        .where('month', '==', monthStr)
+        .where('userId', '==', userId))
+      .valueChanges()
+      .pipe(
+        map(docs => docs.length > 0 ? docs[0] : null)
+      )
+    );
 
-    // map through the items of the query
-    snapshot.pipe(
-      map(docs => {
-        if (docs.length > 0) {
-          const doc = docs[0];
-          // update existing expense
-        } else {
-          // create new expense
+    if(snapshot) {
+      const docId = snapshot.id;
+      await this.fstore.collection('Expenses').doc(docId).update({
+        transactions: arrayUnion(transaction),
+      });
+    } else {
+      const id = await this.fstore.collection('Expenses').ref.doc().id;
+      const expense: Expense = {
+        id,
+        userId,
+        month: monthStr,
+        guests: [],
+        transactions: [transaction],
+        summary: {
+          spent: transaction.amount,
+          storeCount: 1,
+          transactionCount: 1
         }
-      })
-    )
-    
+      };
+      await this.fstore.collection('Expenses').doc(id).set(expense);
+    }
+
     // return await this.fstore.collection('Expenses')
   }
 
